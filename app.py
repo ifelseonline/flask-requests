91b7c23942a1bbe1363e8dd7cf6b003cf9246a5e
Eduardo Nascimento <ifelseonlinebr@gmail.com>
2020-05-14 15:44:11 -0300

    Recebendo todos os pokemons

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from flask import Flask, render_template, request, redirect
+import requests
+import json
+
 app = Flask(__name__)
 
-pokemons = [
-    ['Bulbasaur', 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png'],
-    ['Charmander', 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png'],
-    ['Squirtle', 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/007.png'],
-    ['Pikachu', 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/025.png']
-]
+r = requests.get('https://gist.githubusercontent.com/the-akira/4f2244540d61f8a9d3ec73b280af5f9b/raw/78d015d9d61024dc80c2e6c3c66d455b6b32ed3c/pokedex.json')
+dados = r.text
+
+pokemons = json.loads(dados)
 
 @app.route('/')
 def index():
